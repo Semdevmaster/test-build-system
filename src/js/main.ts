88b79e601c59fdf3968a72fdc5cebe1dd1868365
Semdev1986 <arsentver@gmail.com>
2019-08-12 20:28:30 +0300

    any changes

--- src/js/main.ts
+++ src/js/main.ts
@@ -2,7 +2,7 @@ import summ from './modules/summ.js'
 
 const name = 'Hello Sem'
 console.log(name)
-summ(2, 7)
+summ(2, 10)
 
 document.addEventListener('click', ({ target }) => {
   console.log(target)
