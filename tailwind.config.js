6e31b9450e8b764b6fe09b796f8303e55b43dab7
Semyon <arsentver@gmail.com>
2019-10-15 16:02:20 +0300

    update deps and convert grid.css into tailwind plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -493,5 +493,24 @@ module.exports = {
   corePlugins: {
     preflight: false
   },
-  plugins: []
+  plugins: [
+    require('./tailwind-plugins/transform/rotate')(),
+    require('./tailwind-plugins/grid-layout')({
+      grids: [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+      gaps: {
+        px: '1px',
+        0: '0',
+        1: '0.25rem',
+        2: '0.5rem',
+        3: '0.75rem',
+        4: '1rem',
+        5: '1.25rem',
+        6: '1.5rem',
+        8: '2rem',
+        10: '2.5rem',
+        12: '3rem',
+        16: '4rem',
+      }
+    })
+  ]
 }
